8978a582c40e99f8690b610f74d5c500541f1c89
root <sakshamsaini18@gmail.com>
2025-03-20 23:44:32 +0000

    Improve prompt

--- src/routes/compose.py
+++ src/routes/compose.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import APIRouter, Body, Depends, HTTPException, Query
 from fastapi.responses import StreamingResponse
 from src.database.db import get_db
+from src.database import User
 from src.database.vectory_db import VectorDB
 from src.routes.middleware import get_user_id
 
@@ -20,7 +21,7 @@ async def create_suggestion(
     with get_db() as db:
         if user_id != user.get("user_id"):
             raise HTTPException(status_code=401, detail="Unauthorized")
-
+        user = db.query(User).filter(User.id == user_id).first()
         filter = {}
         query = ""
         if email_id and len(email_id) > 0:
@@ -32,10 +33,6 @@ async def create_suggestion(
             query += f", body: {body} "
 
         return StreamingResponse(
-            pinecone.suggest(
-                user_id=user_id,
-                query=query,
-                filter=filter,
-            ),
+            pinecone.suggest(user_id=user_id, query=query, filter=filter, name=user.name),
             media_type="application/json",
         )
