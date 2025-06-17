ba912348a9a2b1021f572c7083bdfb19697f1982
root <sakshamsaini18@gmail.com>
2025-06-17 12:44:25 +0000

    comment out middleware

--- src/routes/middleware.py
+++ src/routes/middleware.py
@@ -2,7 +2,7 @@ import jwt
 from fastapi import HTTPException, Request
 from jose import JWTError
 
-from src.libs.const import SECRET_KEY, TELNYX_SECRET_KEY
+from src.libs.const import SECRET_KEY
 
 ALGORITHM = "HS256"
 
@@ -22,7 +22,7 @@ def get_user_id(request: Request):
         raise HTTPException(status_code=401, detail="Invalid token")
 
 
-def check_secret_token(request: Request):
-    secret_token = request.headers.get("X-Secret-Token")
-    if secret_token != TELNYX_SECRET_KEY:
-        raise HTTPException(status_code=401, detail="Invalid secret token")
+# def check_secret_token(request: Request):
+#     secret_token = request.headers.get("X-Secret-Token")
+#     if secret_token != TELNYX_SECRET_KEY:
+#         raise HTTPException(status_code=401, detail="Invalid secret token")
