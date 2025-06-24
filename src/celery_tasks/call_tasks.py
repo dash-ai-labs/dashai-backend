22ab796e30683cfed0751d26dc26642d2ff540f9
root <sakshamsaini18@gmail.com>
2025-06-24 00:20:21 +0000

    get all unread emails for past 24 hours

--- src/celery_tasks/call_tasks.py
+++ src/celery_tasks/call_tasks.py
@@ -29,85 +29,41 @@ def prepare_email_brief(phone_number: str, call_control_id: str, call_session_id
         user = db.query(User).filter(User.phone_number == phone_number).first()
 
         if user:
-            if (
-                latest_call_session := db.query(CallSession)
-                .filter(CallSession.user_id == user.id)
-                .order_by(CallSession.created_at.desc())
-                .first()
-            ):
-                new_emails = (
-                    db.query(Email)
-                    .join(EmailAccount, Email.email_account_id == EmailAccount.id)
-                    .filter(
-                        Email.date >= latest_call_session.created_at,
-                        Email.is_read == False,
-                        Email.folder == EmailFolder.INBOX,
-                        EmailAccount.user_id == user.id,
-                    )
-                    .order_by(Email.created_at.desc())
-                    .all()
+            new_emails = (
+                db.query(Email)
+                .filter()
+                .join(EmailAccount, Email.email_account_id == EmailAccount.id)
+                .filter(
+                    Email.is_read == False,
+                    Email.folder == EmailFolder.INBOX,
+                    Email.date >= datetime.now() - timedelta(days=1),
+                    EmailAccount.user_id == user.id,
                 )
-                cache.set(
-                    f"call_control_id_{call_control_id}",
-                    json.dumps(
-                        [
-                            jsonable_encoder(
-                                email.to_dict(
-                                    allowed_columns=[
-                                        "id",
-                                        "sender",
-                                        "sender_name",
-                                        "subject",
-                                        "date",
-                                        "summary",
-                                        "snippet",
-                                    ]
-                                )
+                .order_by(Email.created_at.desc())
+                .all()
+            )
+            cache.set(
+                f"call_control_id_{call_control_id}",
+                json.dumps(
+                    [
+                        jsonable_encoder(
+                            email.to_dict(
+                                allowed_columns=[
+                                    "id",
+                                    "sender",
+                                    "sender_name",
+                                    "subject",
+                                    "date",
+                                    "summary",
+                                    "snippet",
+                                ]
                             )
-                            for email in new_emails
-                        ]
-                    ),
-                    ex=3600,
-                )
-
-                if not cache.get(phone_number):
-                    cache.set(phone_number, user.name)
-            else:
-                new_emails = (
-                    db.query(Email)
-                    .filter()
-                    .join(EmailAccount, Email.email_account_id == EmailAccount.id)
-                    .filter(
-                        Email.is_read == False,
-                        Email.folder == EmailFolder.INBOX,
-                        Email.date >= datetime.now() - timedelta(days=1),
-                        EmailAccount.user_id == user.id,
-                    )
-                    .order_by(Email.created_at.desc())
-                    .all()
-                )
-                cache.set(
-                    f"call_control_id_{call_control_id}",
-                    json.dumps(
-                        [
-                            jsonable_encoder(
-                                email.to_dict(
-                                    allowed_columns=[
-                                        "id",
-                                        "sender",
-                                        "sender_name",
-                                        "subject",
-                                        "date",
-                                        "summary",
-                                        "snippet",
-                                    ]
-                                )
-                            )
-                            for email in new_emails
-                        ]
-                    ),
-                    ex=3600,
-                )
+                        )
+                        for email in new_emails
+                    ]
+                ),
+                ex=3600,
+            )
 
             call_session = CallSession(
                 user_id=user.id,
