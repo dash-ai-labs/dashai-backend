1522a25725570fa1d127948cee9998d0e64a6c11
fjt067316 <63973505+fjt067316@users.noreply.github.com>
2025-07-29 20:40:41 -0400

    Initial heuristic changes for contacts (#23)
    * Initial heuristic changes
    
    * Add is_shown to emails
    
    * update heuristic values

--- src/database/contact.py
+++ src/database/contact.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+from typing import List
 
 from sqlalchemy import (
     UUID,
@@ -7,6 +8,7 @@ from sqlalchemy import (
     DateTime,
     ForeignKey,
     String,
+    Float,
 )
 from sqlalchemy.orm import Session, relationship
 
@@ -23,6 +25,8 @@ class Contact(Base):
 
     email_account_id = Column(UUID, ForeignKey("email_accounts.id"))
     email_account = relationship("EmailAccount", back_populates="contacts")
+    score = Column(Float, default=0.0)
+    alpha = 0.2
 
     def to_dict(self):
         return {
@@ -32,6 +36,7 @@ class Contact(Base):
             "email_account_id": str(self.email_account_id),
             "created_at": self.created_at,
             "updated_at": self.updated_at,
+            "score" : self.score,
         }
 
     @classmethod
@@ -54,3 +59,12 @@ class Contact(Base):
             db.add(contact)
             db.commit()
             return contact
+
+    def increment_score(self, db, value):
+        if not self.score:
+            self.score = 0
+        
+        self.score = self.alpha * value + (1 - self.alpha) * self.score
+        
+        db.add(self)
+        db.commit()
